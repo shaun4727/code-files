87c068f9ea24b7ac31d2abf23c39eb9933dbe6be
shaun4727 <107396795+shaun4727@users.noreply.github.com>
2024-02-05 09:02:57 +0600

    duplicate adjacent numbers

---
+++ remove_adjacent_duplicates.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "azxxzy";
+    int i = 0;
+
+    while (i < s.size()) {
+        if (s[i] == s[i + 1]) {
+            s.erase(i, 2);
+            i = -1;
+        }
+        i++;
+    }
+
+    cout << s;
+    return 0;
+}
